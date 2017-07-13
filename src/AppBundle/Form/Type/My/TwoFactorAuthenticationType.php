e933fc6b4b3e40e83e48b8ab466732f23f2ef616
Borut <bobalazek124@gmail.com>
2017-07-13 22:37:51 +0200

    Some more translations

--- src/AppBundle/Form/Type/My/TwoFactorAuthenticationType.php
+++ src/AppBundle/Form/Type/My/TwoFactorAuthenticationType.php
@@ -23,29 +23,17 @@ class TwoFactorAuthenticationType extends AbstractType
             ->add('tfaEnabled', CheckboxType::class, [
                 'required' => false,
                 'label' => 'Enabled',
-                'attr' => [
-                    'data-help-text' => 'If you want to have the 2FA protection enabled. Note: it will only work, if you have on the the methods set up.',
-                ],
             ])
             ->add('tfaDefaultMethod', TwoFactorMethodType::class, [
                 'label' => 'Default method',
-                'attr' => [
-                    'data-help-text' => 'Which is the default method on the 2FA?',
-                ],
             ])
             ->add('tfaEmailEnabled', CheckboxType::class, [
                 'required' => false,
                 'label' => 'Enable email authentication',
-                'attr' => [
-                    'data-help-text' => 'On each login, you will get an email with a security code, that you will need to type in, before you will be able to fully log into your account.',
-                ],
             ])
             ->add('tfaAuthenticatorEnabled', CheckboxType::class, [
                 'required' => false,
                 'label' => 'Enable authenticator',
-                'attr' => [
-                    'data-help-text' => 'On each login, you will need to enter the code on your authenticator (Google Authenticator, Authy, ...).',
-                ],
             ])
         ;
     }
