d491fbed74c9bf0271f19a2a41b4c27ef41beacf
Borut <bobalazek124@gmail.com>
2017-07-26 20:01:07 +0200

    Yet more docker updates

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,16 +6,19 @@ use Symfony\Component\Debug\Debug;
 if (
     isset($_SERVER['HTTP_CLIENT_IP']) ||
     isset($_SERVER['HTTP_X_FORWARDED_FOR']) ||
+    php_sapi_name() === 'cli-server' ||
     !(
         in_array(
             @$_SERVER['REMOTE_ADDR'],
             [
-                '172.18.0.1', // docker
                 '127.0.0.1',
                 '::1',
             ]
         ) ||
-        php_sapi_name() === 'cli-server'
+        strpos( // docker
+            @$_SERVER['REMOTE_ADDR'],
+            '172.'
+        ) !== false
     )
 ) {
     header('HTTP/1.0 403 Forbidden');
