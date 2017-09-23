6ddcf434c12d8154a55dc1a622c1341f1b29a981
Borut Balazek <bobalazek124@gmail.com>
2017-09-23 18:23:15 +0200

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -9,6 +9,6 @@ include dirname(__FILE__).'/deployer/tasks.php';
 include dirname(__FILE__).'/deployer/hosts.php';
 
 // Include the local stuff if present
-if (file_exists(dirname(__FILE__).'/local.php')) {
+if (file_exists(dirname(__FILE__).'/deployer/local.php')) {
     include dirname(__FILE__).'/deployer/local.php';
 }
